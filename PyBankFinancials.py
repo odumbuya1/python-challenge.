a1dc2d329372e586e86dab3508f132fbe25ac541
odumbuya1 <132717049+odumbuya1@users.noreply.github.com>
2023-10-05 21:44:03 -0400

    Final upload of challenge

---
+++ PyBankFinancials.py
@@ -0,0 +1,74 @@
+# Import Dependencies
+import os
+import csv
+
+# Create file path
+file_to_load = os.path.join ('Resources', 'budget_data.csv')
+file_to_output = os.path.join('analysis', 'budget_analysis.txt')
+
+
+# Track various financial parameters'
+total_months = 0
+month_of_change = []
+net_change_list = []
+greatest_increase = ["", 0]
+greatest_decrease = ["", 9999999999999999999]
+total_net = 0
+
+# open and read file 
+with open(file_to_load) as budget_data:
+    reader = csv.reader(budget_data, delimiter=',')
+
+    header = next(reader)
+
+    # separate header to avoid lurking into it
+    first_row = next(reader)
+    total_months += 1
+    total_net += int(first_row[1])
+    prev_net = int(first_row[1])
+
+    for row in reader:
+        # track totals
+        total_months += 1
+        total_net += int(row[1])
+
+        # track net change
+        net_change = int(row[1]) - prev_net
+        prev_net = int(row[1])
+        net_change_list += [net_change]
+        month_of_change += [row[0]]
+
+
+         # Calculate the greatest increase
+        if net_change > greatest_increase[1]:
+            greatest_increase[0] = row[0]
+            greatest_increase[1] = net_change
+
+        # Calculate the greatest decrease
+        if net_change < greatest_decrease[1]:
+            greatest_decrease[0] = row[0]
+            greatest_decrease[1] = net_change
+
+# Calculate the Average Net Change
+net_monthly_avg = sum(net_change_list) / len(net_change_list)
+
+# Generate Output Summary
+output = (
+    f"Financial Analysis\n"
+    f"----------------------------\n"
+    f"Total Months: {total_months}\n"
+    f"Total: ${total_net}\n"
+    f"Average Change: ${net_monthly_avg:.2f}\n"
+    f"Greatest Increase in Profits: {greatest_increase[0]} (${greatest_increase[1]})\n"
+    f"Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})\n")
+
+# Print the output (to terminal)
+print(output)
+
+# Export the results to text file
+with open(file_to_output, "w") as budget_analysis:
+    budget_analysis.write(output)
+
+        
+
+    
